dcf11d36bdae6eb568ac2cea9bc93a5219e1b940
taeuk <teddyok1206@snu.ac.kr>
2025-04-11 12:52:34 +0900

    Completed attendancee task

--- calculator.cpp
+++ calculator.cpp
@@ -1,18 +1,45 @@
 #include "calculator.h"
 #include <stdexcept>
+#include <limits>
 
 int Calculator::add(int a, int b) {
-    // TODO
+    if ((b > 0 && a > std::numeric_limits<int>::max() - b) || (b < 0 && a < std::numeric_limits<int>::min() - b))
+    {
+        throw std::overflow_error("Over/underflow of integer");
+    }
+    else
+    return a + b;
 }
 
 int Calculator::sub(int a, int b) {
-    // TODO
+    if ((b < 0 && a > std::numeric_limits<int>::max() + b) || (b > 0 && a < std::numeric_limits<int>::min() + b))
+    {
+        throw std::overflow_error("Over/underflow of integer");
+    }
+    else
+        return a - b;
 }
 
 int Calculator::mul(int a, int b) {
-    // TODO
+    if ((b > 1 && a > 0 && b > std::numeric_limits<int>::max() / a) || (b > 1 && a < 0 && b > std::numeric_limits<int>::min() / a))
+    {
+        throw std::overflow_error("Over/underflow of integer");
+    }
+    else
+        return a * b;
 }
 
 int Calculator::div(int a, int b) {
-    // TODO
+    if (b == 0)
+    {
+        throw std::invalid_argument("Division by zero");
+    }
+    else if (a == std::numeric_limits<int>::min() && b == -1)
+    {
+        throw std::overflow_error("Over/underflow of integer");
+    }
+    else
+    {
+        return a / b;
+    }
 }
